b969592fb48db51b646454dcb8ce43d973987155
Igor Santiago <igor.santiago546@outlook.com>
2023-01-20 00:15:40 -0300

    Adicionando tipos de Var

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,17 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("TI92, Começou agora!!!!");
+            //Console.WriteLine("TI92, Começou agora!!!!");
+            int idade = 25;
+            double medida = 15.98;
+            sbyte x = 127;
+            byte y = 25;
+            short z = 6500;
+            uint i = 0;
+            ulong k = 0;
+            
+            
+            
             Console.ReadKey();
         }
     }
